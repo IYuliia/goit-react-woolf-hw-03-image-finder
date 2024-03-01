f929d27a59e23235528c7e100ab2608e3bbd9f00
IYuliia <catnew2012@gmail.com>
2024-03-01 16:09:25 +0100

    updated app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,11 +18,20 @@ class App extends Component {
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    const { query: prevQuery } = prevState;
-    const { query } = this.state;
-
-    if (query !== prevQuery) {
-      await this.handleSearchSubmit(query);
+    if (
+      this.state.query !== prevState.query ||
+      prevState.currentPage !== this.state.currentPage
+    ) {
+      try {
+        const { query, currentPage } = this.state;
+        const data = await getAllImagesApi(query, currentPage);
+        this.setState(prevState => ({
+          images: [...prevState.images, ...data.hits],
+          hasMoreImages: data.hits.length === 12,
+        }));
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
     }
   }
 
@@ -30,30 +39,13 @@ class App extends Component {
     this.setState({ modalImageUrl: imageUrl, isModalOpen: true });
   };
 
-  handleLoadMore = async () => {
-    const { currentPage, query, hasMoreImages } = this.state;
+  handleLoadMore = () => {
+    const { currentPage, hasMoreImages } = this.state;
     if (!hasMoreImages) return;
 
-    const nextPage = currentPage + 1;
-    try {
-      this.setState({ isLoading: true });
-      const data = await getAllImagesApi(query, nextPage);
-      if (data.hits.length < 12) {
-        this.setState({
-          images: [...this.state.images, ...data.hits],
-          hasMoreImages: false,
-        });
-      } else {
-        this.setState({
-          images: [...this.state.images, ...data.hits],
-          currentPage: nextPage,
-        });
-      }
-    } catch (error) {
-      console.error('Error fetching images:', error);
-    } finally {
-      this.setState({ isLoading: false });
-    }
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1,
+    }));
   };
 
   handleSearchSubmit = async query => {
@@ -63,13 +55,6 @@ class App extends Component {
       currentPage: 1,
       hasMoreImages: true,
     });
-
-    try {
-      const data = await getAllImagesApi(query);
-      this.setState({ images: data.hits });
-    } catch (error) {
-      console.error('Error fetching images:', error);
-    }
   };
 
   render() {
